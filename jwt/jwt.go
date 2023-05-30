ded3b90e6d717b3cf6eeabd7bef555a8c21b3d3b
Abhinand KR <nanduttanvsabhi@gmail.com>
2023-05-30 15:06:50 +0530

    basic jwt custom claims

---
+++ jwt/jwt.go
@@ -0,0 +1,32 @@
+package jwt
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	// "github.com/golang-jwt/jwt/v4"
+)
+
+// in case of HMAC same key is used for signing as well as to check whether the token is valid
+// in all the other signing method provided my go, there will be a private kry used for signing and a public key which is used to check the validity of the key
+// the other one that i mentioned above was ECDSA and RSA which are kind of identical
+
+type userClaims struct {
+	jwt.StandardClaims
+	SessionID int64
+}
+
+func (u *userClaims) Valid() error {
+	
+	if !u.VerifyExpiresAt(time.Now().Unix(),true) {
+		return fmt.Errorf("token has expired")
+	}
+
+	if u.SessionID == 0 {
+		return fmt.Errorf("invalid session ID")
+	}
+
+	return nil
+}
+ 
--- main.go
+++ main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"fmt"
 
-	"github.com/abhinandkakkadi/golangbasics/handler"
+	"github.com/abhinandkakkadi/golangbasics/errorhandler"
 )
 
 func main() {
 
 	fmt.Println("going to handle a panic using defer")
-	handler.DeferExample()
+	errorhandler.DeferExample()
 	fmt.Println("this code is printed since the error is handled")
+
 }	
