8730f5a4a1c2c0fbbb1caff3107075fe516b2c05
Abhinand KR <nanduttanvsabhi@gmail.com>
2023-05-31 21:08:22 +0530

    added struct embeding

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/abhinandkakkadi/golangbasics/errorhandler"
+	"github.com/abhinandkakkadi/golangbasics/structembed"
 )
 
 func main() {
@@ -11,5 +12,8 @@ func main() {
 	fmt.Println("going to handle a panic using defer")
 	errorhandler.DeferExample()
 	fmt.Println("this code is printed since the error is handled")
+
+	structembed.StructEmbeding()
+	
 
 }	
